9ff9ecd8a1f162e852af9b8b8d0272d7b221ad09
Roney Banerjee <ronban@wlabs.cloud>
2021-11-06 16:24:42 -0500

    Updated Ingress to create certificate in container

--- config/ingress/create-certs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-read -p 'Enter server name: ' server_name
-sudo rm -rf certs
-mkdir certs
-openssl genrsa -out certs/${server_name}.key 2048
-openssl req -new -key certs/${server_name}.key -out certs/${server_name}.csr
-openssl x509 -req -days 365 -in certs/${server_name}.csr -signkey certs/${server_name}.key -out certs/${server_name}.crt
-rm -f certs/${server_name}.csr
